26df2046b3697590a1ebbe0ad055e958b7ec6bf5
oivan <oivan@sofserveinc.com>
2013-09-30 14:53:06 +0300

    self_rateable on: :sdf1, by: :sdf done

--- lib/self_rateable/rateable.rb
+++ lib/self_rateable/rateable.rb
@@ -1,11 +1,12 @@
 module SelfRateable
   module Rateable
-    def self_rateable(options = {})
+    def self_rateable(options = {on: :rating, by: :user})
       class_eval do
         has_many :ratings, :as => :rateable, :dependent => :destroy, :class_name => 'SelfRateable::Rating'
+        has_many :raters, as: :rater, :class_name => options[:by].to_s.camelize
         include SelfRateable::Core::InstanceMethods
         extend SelfRateable::Core::ClassMethods
-      end
+      end      
     end
   end
 end
--- lib/self_rateable/rating.rb
+++ lib/self_rateable/rating.rb
@@ -1,6 +1,6 @@
 module SelfRateable
   class Rating < ActiveRecord::Base
     belongs_to :rateable, :polymorphic => true
-    belongs_to :user
+    belongs_to :rater
   end
 end
