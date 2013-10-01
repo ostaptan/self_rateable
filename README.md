e8ffa86e662b4dba7a7d1f0ae3575d647fa9b154
oivan <oivan@sofserveinc.com>
2013-10-01 15:24:01 +0300

    first release

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SelfRateable
 
 Clean, powerful, customizable, simple, configurable, gem for displaying and handling ratings, likes, and feedbacks for ActiveRecord model.
+People!!! Please provide your feedbacks what functionality to add on the issues page. I'm open to whatever questions for improving this gem. 
 
 ## Installation
 
@@ -16,7 +17,7 @@ Or install it yourself as:
 
     $ gem install self_rateable
 
-## Usage(still developing)
+## Usage
 
 Make you ActiveRecord model self rateable:
 
@@ -32,11 +33,19 @@ Rates philosophy as in facebook & vk (like/unlike)
     	self_rateable by: :class_name_of_AR_model_that_can_rate, type: :likes
  	end
 
+ 	Rate:
+
+ 	object.like object_of_AR_model_that_can_rate
+
 Stars rates diapasone 1..5 (as on movies) 	
 
  	class Model < ActiveRecord::Base
     	self_rateable by: :class_name_of_AR_model_that_can_rate, type: :stars
  	end
+
+ 	Rate:
+
+ 	object.rate object_of_AR_model_that_can_rate, count_of_stars
 
  Rates philosophy as in stackoverflow (+1 vs -1)	
 
@@ -44,13 +53,18 @@ Stars rates diapasone 1..5 (as on movies)
     	self_rateable by: :class_name_of_AR_model_that_can_rate, type: :points
  	end
 
+ 	Rate:
+
+ 	object.vote object_of_AR_model_that_can_rate, bool
+
+This vote method returns false if object_of_AR_model_that_can_rate has already voted once. You can capture that output to show some messages.	
+
+ (If true it adds 1 point if false it decreases 1 point)	
+
  Run rake task to prepare and update your database:
 
  	rake self_rateable	
 
- Use: (still not developed)
-
- 	object.rate object_of_AR_model_that_can_rate, count_of_rates
 
 ## Contributing
 
@@ -58,4 +72,10 @@ Stars rates diapasone 1..5 (as on movies)
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
-5. Create new Pull Request
+5. Create new Pull Request and send it to me.)
+
+## What to do?
+
+1. Create generators for templates to render on UI with images and stylings of different types of ratings.
+2. Create complete test coverage
+3. Add more flexibility by providing more class and instance methods. 
--- lib/self_rateable/core/instance_methods.rb
+++ lib/self_rateable/core/instance_methods.rb
@@ -1,19 +1,19 @@
 module SelfRateable::Core
  	module InstanceMethods
  	  module Likes
-	 	  def rating
+	 	  def likes
 	        ratings.sum(:rating).to_f
 	      end
 
-	      def rated_by_rater?(rater)
+	      def liked_by_rater?(rater)
 	        ratings.where(:rater_id => rater.id).count > 0
 	      end
 
-	      def rate(rater)
+	      def like(rater)
 	      	rater.present? ? rate = ratings.where(rater_id: rater.id).first : raise('Rater object is incorrect.')
 	      	if rate.present?
 	      	# unlike if already rated 
-	      	 	rated_by_rater?(rater) ? ratings.where(rater_id: rater.id).delete_all : rate.rating += 1
+	      	 	liked_by_rater?(rater) ? ratings.where(rater_id: rater.id).delete_all : rate.rating += 1
 	      	 	rate.save!
 	      	else
 	      	#create new record if not yet rated	
@@ -25,9 +25,56 @@ module SelfRateable::Core
 	  end
 
 	  module Stars
+	  	  def stars
+	        ratings.average(:rating).to_f
+	      end
+
+	      def rated_by_rater?(rater)
+	        ratings.where(:rater_id => rater.id).count > 0
+	      end
+
+	      def rate(rater, stars)
+	      	rater.present? ? rate = ratings.where(rater_id: rater.id).first : raise('Rater object is incorrect.')
+	      	stars = 5 if stars > 5
+	      	stars = 0 if stars < 1 
+	      	if rated_by_rater?(rater)
+	      	# set new star rating if already rated
+	      	 	rate.rating = stars.to_i
+	      	 	rate.save!
+	      	else
+	      	#create new record if not yet rated	
+	      		ratings << SelfRateable::Rating.new(rating: stars.to_i, rater_id: rater.try(:id))
+	      	end
+	      	self.save!
+	      end
 	  end
 
 	  module Points
+	  	  def votes
+	        ratings.sum(:rating).to_f
+	      end
+
+	      def voted_by_rater?(rater)
+	        ratings.where(:rater_id => rater.id).count > 0
+	      end
+
+	      def vote(rater, val)
+	      	value = parse_val(val)
+	      	if voted_by_rater?(rater)
+	      	# set new star rating if already rated
+	      	 	return false
+	      	else
+	      	#create new record if not yet rated	
+	      		ratings << SelfRateable::Rating.new(rating: value.to_i, rater_id: rater.try(:id))
+	      	end
+	      	self.save!
+	      end
+
+	      private
+
+	      def parse_val(val)
+	      	val ? 1 : -1
+	      end
 	  end
  	end
 end
